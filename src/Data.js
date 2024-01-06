dc554dc64d080d54ae7ec59ed9ca1ef2892ac09c
Anjum <2021580@iub.edu.bd>
2024-01-06 22:07:48 +0600

    first commit

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Summer Outfit",
+      price: 25,
+      desc: "Summer friendly outfits with comfort and trendy fashion.",
+      img: "images/tops-3.jpg",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Winter Outfit",
+      price: 50,
+      desc: "Winter fancy attire with fashionable jacket,shirt.",
+      img: "images/p-8.png",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "Spring Attire",
+      price: 35,
+      desc: "Enjoy Spring breeze season super friendly dress",
+      img: "images/tops-6.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Casual Outfit",
+      price: 45,
+      desc: "Daily outing with comforting dress and varieties.",
+      img: "images/tops-7.jpg",
+    },
+  ];
